7e0d42f7bb850519397ff7fd37e729d5dd2451d1
Piotr Machowski <PiotrMachowski@users.noreply.github.com>
2021-06-15 09:50:18 +0200

    Fix current room calculation for Xiaomi vacuums

--- custom_components/xiaomi_cloud_map_extractor/xiaomi/map_data_parser.py
+++ custom_components/xiaomi_cloud_map_extractor/xiaomi/map_data_parser.py
@@ -116,8 +116,8 @@ class MapDataParserXiaomi(MapDataParser):
         image_top = MapDataParserXiaomi.get_int32(header, block_header_length - 16)
         image_left = MapDataParserXiaomi.get_int32(header, block_header_length - 12)
         image_width = MapDataParserXiaomi.get_int32(header, block_header_length - 4)
-        x = round(map_to_image(vacuum_position.x) - image_left)
-        y = round(map_to_image(vacuum_position.y) - image_top)
+        x = round(MapDataParserXiaomi.map_to_image(vacuum_position.x) - image_left)
+        y = round(MapDataParserXiaomi.map_to_image(vacuum_position.y) - image_top)
         room = ImageHandlerXiaomi.get_room_at_pixel(data, image_width, x, y)
         return room
 
